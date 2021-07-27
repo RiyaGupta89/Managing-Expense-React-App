37cb058bebfd3992e56a087e2b44a10ee62910bf
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-07-27 20:12:18 +0530

    Added expenses component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses";
 
 function App() {
 
@@ -37,12 +37,7 @@ function App() {
   return (
     <div className="App">
       <h1>Let's get started!!</h1>
-      <ExpenseItem date={itemArray[0].date} title={itemArray[0].title} price={itemArray[0].price}/>
-      <ExpenseItem date={itemArray[1].date} title={itemArray[1].title} price={itemArray[1].price}/>
-      <ExpenseItem date={itemArray[2].date} title={itemArray[2].title} price={itemArray[2].price}/>
-      <ExpenseItem date={itemArray[3].date} title={itemArray[3].title} price={itemArray[3].price}/>
-      <ExpenseItem date={itemArray[4].date} title={itemArray[4].title} price={itemArray[4].price}/>
-      <ExpenseItem date={itemArray[5].date} title={itemArray[5].title} price={itemArray[5].price}/>
+      <Expenses itemArray={itemArray}/>
     </div>
   );
 }
---
+++ src/Components/Expenses.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css"
+
+const Expenses = ({itemArray}) => {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        date={itemArray[0].date}
+        title={itemArray[0].title}
+        price={itemArray[0].price}
+      />
+      <ExpenseItem
+        date={itemArray[1].date}
+        title={itemArray[1].title}
+        price={itemArray[1].price}
+      />
+      <ExpenseItem
+        date={itemArray[2].date}
+        title={itemArray[2].title}
+        price={itemArray[2].price}
+      />
+      <ExpenseItem
+        date={itemArray[3].date}
+        title={itemArray[3].title}
+        price={itemArray[3].price}
+      />
+      <ExpenseItem
+        date={itemArray[4].date}
+        title={itemArray[4].title}
+        price={itemArray[4].price}
+      />
+      <ExpenseItem
+        date={itemArray[5].date}
+        title={itemArray[5].title}
+        price={itemArray[5].price}
+      />
+    </div>
+  );
+};
+
+export default Expenses;
