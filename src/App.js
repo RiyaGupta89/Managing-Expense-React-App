8c5c448fc76ff8f37bfa9a2333c3079c98196dba
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-07-27 19:12:14 +0530

    Added day year and month seperately

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
+
+  const itemArray = [
+    {
+      date: new Date(2021, 3, 26),
+      title: "Scooty Insurance",
+      price: 159.75,
+    },
+    {
+      date: new Date(2021, 9, 8),
+       title:"Birthday Cake",
+        price: 52.35
+    },{
+      date: new Date(2021, 12, 24),
+       title:"Santa Gifts",
+        price: 119.21
+    },
+    {
+      date: new Date(2021, 7, 26),
+       title: "Mobile Phone",
+        price: 259.75
+    }, 
+    {
+      date: new Date(2021, 7, 27),
+       title:"Kitkat",
+        price: 159.75 
+    },
+    {
+      date: new Date(2021, 11, 12),
+       title: "College Fees",
+        price: 359.75
+    }
+  ]
+
   return (
     <div className="App">
       <h1>Let's get started!!</h1>
-      <ExpenseItem />
+      <ExpenseItem date={itemArray[0].date} title={itemArray[0].title} price={itemArray[0].price}/>
+      <ExpenseItem date={itemArray[1].date} title={itemArray[1].title} price={itemArray[1].price}/>
+      <ExpenseItem date={itemArray[2].date} title={itemArray[2].title} price={itemArray[2].price}/>
+      <ExpenseItem date={itemArray[3].date} title={itemArray[3].title} price={itemArray[3].price}/>
+      <ExpenseItem date={itemArray[4].date} title={itemArray[4].title} price={itemArray[4].price}/>
+      <ExpenseItem date={itemArray[5].date} title={itemArray[5].title} price={itemArray[5].price}/>
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import "./ExpenseItem.css"
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });   
+    const year = props.date.getFullYear();
+
     return (
         <div className="expense-item">
-            <div>July 26 2021</div>
+            <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+            </div>
             <div className="expense-item__description">
-                <h2>Scooty Insurance</h2>
-                <div className="expense-item__price">$296.43</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.price}</div>
             </div>
         </div>
     )
